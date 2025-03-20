d03ee343ff36c065bc4364328b8af54c8ac3a14d
A <a@b.com>
2025-03-19 23:09:51 -0700

    Initial setup.sh for the project

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+[[ $_ != $0 ]] && echo "Setting up the devel environment" || { echo "Please source this script:\n>> source setup.sh"; exit -1; }
+
+source ~/.bash_profile
+
+export DEVEL_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+export DEVEL_VENV="learnpycli"
+
+# Python setup
+workon $DEVEL_VENV
+cd $DEVEL_ROOT
+echo 'DEVEL_ROOT="'$DEVEL_ROOT'"'
+export PYTHONPATH=$DEVEL_ROOT/pysrc:$DEVEL_ROOT/pysrc/pb:$PYTHONPATH
+
+# Golang setup
+gvm use go1.22.3
+export GOPATH=$DEVEL_ROOT/go
+export PATH=$PATH:$GOPATH/bin
+
